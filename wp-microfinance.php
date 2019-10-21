2d13fc092ec9632567cdf881b8d27c62358f7e6d
MD SAMI <mdsami.diu@gmail.com>
2019-10-22 00:53:02 +0600

    first commit

---
+++ wp-microfinance.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+Plugin Name: Wp Microfinance
+Plugin URI: https://wptasker.com
+Description: Microfinance WordPress Plugin .
+Version: 1.0
+Author: Team WP Tasker
+Author URI: https://wptasker.com
+Text Domain: wp-microfinance
+License:  GPL v3
+*/
+
+// Disallow direct access
+if (!function_exists("add_action")) {
+    echo "Don't call me, I'll call you.";
+    exit;
+}
